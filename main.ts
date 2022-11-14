9427336871622af589f2316d1897986aa1a0a03d
alha501 <113909973+alha501@users.noreply.github.com>
2022-11-14 15:00:37 -0500

    door alarm

--- main.ts
+++ main.ts
@@ -1,13 +1,50 @@
-input.onGesture(Gesture.FreeFall, function () {
-    count += 1
-    basic.showNumber(count)
+// stay button
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("stay")
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
 })
-let count = 0
-basic.showLeds(`
-    . . . . .
-    . . # . .
-    . # . # .
-    . . # . .
-    . . . . .
-    `)
-count = 0
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "stay") {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(500)
+        basic.clearScreen()
+    } else if (receivedString == "away") {
+        basic.showIcon(IconNames.No)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+// away button
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("away")
+    basic.clearScreen()
+    basic.showIcon(IconNames.No)
+})
+radio.setGroup(1)
+let alarm = music.createSoundEffect(
+WaveShape.Square,
+5000,
+3662,
+255,
+220,
+5000,
+SoundExpressionEffect.None,
+InterpolationCurve.Linear
+)
+basic.forever(function () {
+    if (input.acceleration(Dimension.Strength) > 30) {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Square,
+        5000,
+        3662,
+        255,
+        220,
+        5000,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.UntilDone)
+    } else if (false) {
+    	
+    }
+})
